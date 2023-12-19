bb4793d0ffb89831b1421698997bbe0ae49a75a7
Manoj <rayimanoj8@gmail.com>
2023-12-19 23:30:43 +0530

    Added a allowed hosts as .vercel.app

--- library/settings.py
+++ library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7htr%_$jy5z@t(ya1yk_jitrt-savdmeqbs4*@-ms8h5828a)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
